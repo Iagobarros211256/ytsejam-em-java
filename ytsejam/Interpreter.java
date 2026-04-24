d02a8d1193d8646e6baae4b24bb8797801a8ebe3
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2026-04-24 00:35:07 -0300

    implemented a basic 4 mathematical operations logic

--- ytsejam/Interpreter.java
+++ ytsejam/Interpreter.java
@@ -35,5 +35,33 @@ class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right); 
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                } 
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;    
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Unreachable coondition.
+        return null;
+    }
     
 }
